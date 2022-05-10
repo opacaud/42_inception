6c93916bb17ebd457539b154b07a0638c2922551
Olympe Pacaud <olympepacaud@gmail.com>
2022-05-10 16:53:05 +0200

    why aren't variables expanded in setup.sql?

--- Makefile
+++ Makefile
@@ -105,10 +105,10 @@ export $(shell sed 's/=.*//' srcs/.env)
 all: up
 
 up:
-	sudo mkdir -p /home/opacaud/data/mariadb
-	echo "/home/opacaud/data/mariadb folder created"
-	sudo mkdir -p /home/opacaud/data/wordpress_nginx
-	echo "/home/opacaud/data/wordpress_nginx folder created"
+	@sudo mkdir -p /home/opacaud/data/mariadb
+	@echo "/home/opacaud/data/mariadb folder created"
+	@sudo mkdir -p /home/opacaud/data/wordpress_nginx
+	@echo "/home/opacaud/data/wordpress_nginx folder created"
 	docker-compose -f ${YML} up -d --build
 
 down:
@@ -119,25 +119,4 @@ fclean:
 
 re:	fclean all
 
-.PHONY: all build up down fclean re
-
-
-# include srcs/.env
-# export $(shell sed 's/=.*//' srcs/.env)
-
-# up:
-# 	cd srcs && docker-compose up --build -d --remove-orphans && cd ..
-
-# down:
-# 	cd srcs && docker-compose down -t 2 && cd ..
-# 	@rm -rf ~/data/mariadb
-
-# fdown:
-# 	cd srcs && docker-compose down -t 2 -v && cd ..
-# 	@rm -rf ~/data
-
-# clean:
-# 	docker-compose down --rmi all
-	
-
-# .PHONY: up down fdown clean
+.PHONY: all up down fclean re
--- srcs/requirements/mariadb/setup/setup.sql
+++ srcs/requirements/mariadb/setup/setup.sql
@@ -20,7 +20,7 @@ CREATE USER 'opacaud'@'localhost';
 
 -- https://mariadb.com/kb/en/set-password/
 -- "The SET PASSWORD statement assigns a password to an existing MariaDB user account."
-SET PASSWORD FOR 'opacaud'@'localhost' = PASSWORD('Pizza4fromages');
+SET PASSWORD FOR 'opacaud'@'localhost' = PASSWORD('$MYSQL_USER_PWD');
 
 -- https://mariadb.com/kb/en/grant/
 -- usage: "GRANT priv_type [...] ON [...] priv_level TO user_specification [user_options]"
@@ -28,8 +28,8 @@ SET PASSWORD FOR 'opacaud'@'localhost' = PASSWORD('Pizza4fromages');
 -- priv_level: " * | *.* | db_name.* | db_name.tbl_name | tbl_name | db_name.routine_name [...] Database privileges priv_type are granted using db_name.* for priv_level, or using just * to use default database."
 -- user_specification: "username [authentication_option]"
 -- authentication_option: IDENTIFIED BY 'password'
-GRANT ALL PRIVILEGES ON wordpress.* TO 'opacaud'@'localhost' IDENTIFIED BY 'Pizza4fromages';
-GRANT ALL ON wordpress.* TO 'opacaud'@'%' IDENTIFIED BY 'Pizza4fromages';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'opacaud'@'localhost' IDENTIFIED BY '$MYSQL_USER_PWD';
+GRANT ALL ON wordpress.* TO 'opacaud'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';
 
 -- https://mariadb.com/kb/en/flush/#:~:text=The%20FLUSH%20statement%20clears%20or,statement%20is%20similar%20to%20FLUSH%20.
 -- "The FLUSH statement clears or reloads various internal caches used by MariaDB."
@@ -37,7 +37,7 @@ GRANT ALL ON wordpress.* TO 'opacaud'@'%' IDENTIFIED BY 'Pizza4fromages';
 FLUSH PRIVILEGES;
 
 -- creating the admin user whose username can't be admin
-CREATE USER 'boss'@'localhost' IDENTIFIED BY 'Pizza5fromages';
+CREATE USER 'boss'@'localhost' IDENTIFIED BY '$MYSQL_BOSS_PWD';
 
 -- https://mariadb.com/kb/en/grant/
 -- "Global privileges priv_type are granted using *.* for priv_level."
@@ -46,15 +46,17 @@ GRANT ALL ON *.* TO 'boss'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 
-CREATE USER 'boss'@'%' IDENTIFIED BY 'Pizza5fromages';
+CREATE USER 'boss'@'%' IDENTIFIED BY '$MYSQL_BOSS_PWD';
 GRANT ALL ON *.* TO 'boss'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 
 -- https://mariadb.com/kb/en/alter-user/
 -- "The ALTER USER statement modifies existing MariaDB accounts."
--- ALTER USER 'root'@'localhost' IDENTIFIED BY 'MYSQL_ROOT_PWD';
+-- ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
 
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PWD');
 
 FLUSH PRIVILEGES;
+
+SELECT user, host, password FROM mysql.user;
